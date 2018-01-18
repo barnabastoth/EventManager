ef882b2e4f027723ed5d3809604ec5103fa8c914
Unknown <barnabastoth94@gmail.com>
2018-01-18 08:13:39 +0100

    adding speakers has been added to normal event edit

---
+++ src/main/java/application/model/Event/eventField.java
@@ -0,0 +1,4 @@
+package application.model.Event;
+
+public class Field {
+}
--- src/main/java/application/repository/SpeakerRepository.java
+++
@@ -1,9 +0,0 @@
-package application.repository;
-
-import application.model.Event.Speaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeakerRepository extends JpaRepository<Speaker, Long> {
-
-    Speaker findOne(Long id);
-}
