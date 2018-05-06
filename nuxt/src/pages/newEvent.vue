22849bb8ab3628cbf3cc4d6509815dd8f377403c
Unknown <barnabastoth94@gmail.com>
2018-05-06 02:21:07 +0200

    Custom controllable fields on event

--- nuxt/src/pages/newEvent.vue
+++ nuxt/src/pages/newEvent.vue
@@ -5,7 +5,7 @@
     leave-active-class="animated zoomOut"
   >
     <q-page class="flex flex-center" padding style="width: 1000px; max-width: 90vw;">
-      <q-card inline class="q-ma-sm shadow-24" style="padding: 24px;">
+      <q-card inline class="q-ma-sm shadow-24" style="width: 1000px; max-width: 90vw; padding: 24px;">
         <q-item style="margin-bottom: 30px" class="shadow-1 bg-grey-2">
           <q-item-side left>
             <q-item-tile style="font-size: 30px;" color="primary" icon="fa-calendar" />
@@ -14,169 +14,27 @@
             <q-item-tile style="font-size: 30px;" class="text-center" label>Új esemény létrehozása</q-item-tile>
           </q-item-main>
         </q-item>
-        <q-card-media overlay-position="bottom">
-          <img src="statics/parallax2.jpg">
-
-          <q-card-title slot="overlay">
-            <span slot="subtitle">Kép kiválasztása</span>
-          </q-card-title>
-        </q-card-media>
         <q-card-main>
-          <q-item>
+          <q-item class="shadow-2 bg-grey-1" v-for="(field, index) in event" :key="field.subText" style="margin-bottom: 15px;">
             <q-item-side>
-              <q-item-tile color="primary" icon="fa-globe" />
+              <q-item-tile color="primary" :icon="field.icon" />
             </q-item-side>
             <q-item-main>
               <q-field>
-                <q-input v-model="event.title.text" float-label="Esemény címe röviden">
-                  <q-toggle
-                    v-model="event.title.active"
-                    checked-icon="visibility_off"
-                    unchecked-icon="visibility"
-                    style="margin-left: 25px"
-                    true-value="0"
-                    false-value="1"
-                  />
-                </q-input>
+                <q-input v-model="field.subText" float-label="A mező neve"></q-input>
+                <q-input v-model="field.icon" float-label="A mező ikonja"></q-input>
+                <q-input type="textarea" rows="3" v-model="field.text" float-label="A mező szöveges tartalma"></q-input>
               </q-field>
-            </q-item-main>
-          </q-item>
-          <q-item>
-          <q-item-side>
-            <q-item-tile color="primary" icon="fa-address-book" />
-          </q-item-side>
-          <q-item-main>
-            <q-field>
-              <q-input v-model="event.name.text" float-label="Esemény neve hosszabban">
-                <q-toggle
-                  v-model="event.name.active"
-                  checked-icon="visibility_off"
-                  unchecked-icon="visibility"
-                  style="margin-left: 25px"
-                  true-value="0"
-                  false-value="1"
+              <q-field style="margin-top: 3px;">
+                <q-btn-toggle
+                  v-model="field.active"
+                  toggle-color="primary"
+                  :options="[
+                            {label: 'Legyen látható', value: '1'},
+                            {label: 'Ne legyen látható', value: '0'}
+                          ]"
                 />
-              </q-input>
-            </q-field>
-          </q-item-main>
-          </q-item>
-
-          <q-item>
-            <q-item-side>
-              <q-item-tile color="primary" icon="fa-address-book" />
-            </q-item-side>
-            <q-item-main>
-              <q-field>
-                <q-input v-model="event.description.text" type="textarea" rows="2" float-label="Esemény leírása">
-                  <q-toggle
-                    v-model="event.description.active"
-                    checked-icon="visibility_off"
-                    unchecked-icon="visibility"
-                    style="margin-left: 25px"
-                    true-value="0"
-                    false-value="1"
-                  />
-                </q-input>
-              </q-field>
-            </q-item-main>
-          </q-item>
-
-          <q-item>
-            <q-item-side>
-              <q-item-tile color="primary" icon="fa-calendar" />
-            </q-item-side>
-            <q-item-main>
-              <q-field>
-                <q-input v-model="event.date.text" float-label="Esemény időpontja">
-                  <q-toggle
-                    v-model="event.date.active"
-                    checked-icon="visibility_off"
-                    unchecked-icon="visibility"
-                    style="margin-left: 25px"
-                    true-value="0"
-                    false-value="1"
-                  />
-                </q-input>
-              </q-field>
-            </q-item-main>
-          </q-item>
-
-          <q-item>
-            <q-item-side>
-              <q-item-tile color="primary" icon="fa-clock" />
-            </q-item-side>
-            <q-item-main>
-              <q-field>
-                <q-input v-model="event.duration.text" float-label="Esemény időtartalma">
-                  <q-toggle
-                    v-model="event.duration.active"
-                    checked-icon="visibility_off"
-                    unchecked-icon="visibility"
-                    style="margin-left: 25px"
-                    true-value="0"
-                    false-value="1"
-                  />
-                </q-input>
-              </q-field>
-            </q-item-main>
-          </q-item>
-
-          <q-item>
-            <q-item-side>
-              <q-item-tile color="primary" icon="fa-map-marker" />
-            </q-item-side>
-            <q-item-main>
-              <q-field>
-                <q-input v-model="event.address.text" float-label="Esemény címe">
-                  <q-toggle
-                    v-model="event.address.active"
-                    checked-icon="visibility_off"
-                    unchecked-icon="visibility"
-                    style="margin-left: 25px"
-                    true-value="0"
-                    false-value="1"
-                  />
-                </q-input>
-              </q-field>
-            </q-item-main>
-          </q-item>
-
-          <q-item>
-            <q-item-side>
-              <q-item-tile color="primary" icon="fa-ticket-alt" />
-            </q-item-side>
-            <q-item-main>
-              <q-field>
-                <q-input v-model="event.price.text" float-label="Jegy ára">
-                  <q-toggle
-                    v-model="event.price.active"
-                    checked-icon="visibility_off"
-                    unchecked-icon="visibility"
-                    style="margin-left: 25px"
-                    true-value="0"
-                    false-value="1"
-                  />
-                </q-input>
-              </q-field>
-            </q-item-main>
-          </q-item>
-
-          <q-item>
-            <q-item-side>
-              <q-item-tile color="primary" icon="fa-link" />
-            </q-item-side>
-            <q-item-main>
-              <q-field>
-                <q-input v-model="event.ticketLink.text" float-label="Jegyhez link">
-                  <q-toggle
-                    v-model="event.ticketLink.active"
-                    checked-icon="visibility_off"
-                    unchecked-icon="visibility"
-                    style="margin-left: 25px"
-                    true-value="0"
-                    false-value="1"
-                  />
-                </q-input>
+                <q-btn class="float-right" color="warning" icon="fa-trash-alt" @click="deleteField(index)"><q-tooltip>Mező törlése</q-tooltip></q-btn>
               </q-field>
             </q-item-main>
           </q-item>
@@ -189,7 +47,7 @@
               <q-field>
                 <p>Maga az esemény:</p>
                 <q-btn-toggle
-                  v-model="event.active"
+                  v-model="active"
                   toggle-color="primary"
                   :options="[
                             {label: 'Legyen látható', value: '1'},
@@ -200,7 +58,8 @@
             </q-item-main>
           </q-item>
         </q-card-main>
-        <q-btn color="primary" icon-right="fa-sign-in-alt" @click="createNewEvent()" label="Esemény létrehozása" />
+        <q-btn color="info" icon-right="fa-plus" @click="addNewField()" label="Új mező hozzáadása" />
+        <q-btn color="primary" class="float-right" icon-right="fa-sign-in-alt" @click="createNewEvent()" label="Esemény létrehozása" />
       </q-card>
     </q-page>
   </transition>
@@ -216,57 +75,57 @@ export default {
   name: 'newEvent',
   data: function () {
     return {
-      event: {
-        title: {
+      event: [
+        {
           text: '',
-          subText: '',
+          subText: 'Cím',
           active: '1',
           icon: 'fa-address-book'
         },
-        name: {
+        {
           text: '',
-          subText: '',
+          subText: 'Név',
           active: '1',
           icon: 'fa-address-book'
         },
-        address: {
+        {
           text: '',
-          subText: '',
-          active: '1',
-          icon: 'fa-calendar'
-        },
-        description: {
-          text: '',
-          subText: '',
-          active: '1',
-          icon: 'fa-clock'
-        },
-        price: {
-          text: '',
-          subText: '',
+          subText: 'Helyszín',
           active: '1',
           icon: 'fa-map-marker'
         },
-        duration: {
+        {
           text: '',
-          subText: '',
+          subText: 'Leírás',
+          active: '1',
+          icon: 'fa-clock'
+        },
+        {
+          text: '',
+          subText: 'Jegy ár',
           active: '1',
           icon: 'fa-ticket-alt'
         },
-        ticketLink: {
+        {
           text: '',
-          subText: '',
+          subText: 'Időtartalom',
+          active: '1',
+          icon: 'fa-clock'
+        },
+        {
+          text: '',
+          subText: 'Jegy link',
           active: '1',
           icon: 'fa-link'
         },
-        date: {
+        {
           text: '',
-          subText: '',
+          subText: 'Időpontja',
           active: '1',
           icon: 'fa-calendar'
-        },
-        active: '1'
-      }
+        }
+      ],
+      active: '1'
     }
   },
   methods: {
@@ -292,6 +151,35 @@ export default {
             message: 'Valami hiba történt az esemény létrehozásakor.'
           })
         })
+    },
+    addNewField () {
+      if (this.$data.event[this.$data.event.length - 1].subText === 'Új mező') {
+        Notify.create({
+          type: 'info',
+          color: 'info',
+          position: 'bottom',
+          timeout: 3000,
+          message: 'Már kész van egy új mező'
+        })
+      } else {
+        this.$data.event.push({
+          text: '',
+          subText: 'Új mező',
+          active: '1',
+          icon: 'fa-file'
+        })
+      }
+    },
+    deleteField (index) {
+      let name = this.$data.event[index].subText
+      this.$data.event.splice(index, 1)
+      Notify.create({
+        type: 'info',
+        color: 'info',
+        position: 'bottom',
+        timeout: 3000,
+        message: 'Sikeresen törölted a következő mezőt: ' + name
+      })
     }
   }
 }
