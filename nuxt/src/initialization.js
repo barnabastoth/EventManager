75599a48fedc21b2b8f1ddd6d925599ffcfd683d
Unknown <barnabastoth94@gmail.com>
2018-05-25 10:27:33 +0200

    Nuxt folder moved

--- nuxt/src/initialization.js
+++
@@ -1,3 +0,0 @@
-export default ({ app, router, store, Vue }) => {
-  alert('anyad')
-}
--- nuxt/src/plugins/autoAuthentication.js
+++
@@ -1,8 +0,0 @@
-import AXIOS from 'axios'
-
-export default ({ app, router, store, Vue }) => {
-  AXIOS.defaults.baseURL = 'http://localhost:8089'
-  store.dispatch('logInUserWithToken').then(() => {
-    store.dispatch('loadSiteSettings')
-  })
-}
